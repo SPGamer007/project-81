d41e5e2723d7925e1c7431a8d581048beccd34b3
SPGamer007 <129699948+SPGamer007@users.noreply.github.com>
2024-06-27 11:03:40 +1000

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+
+export default class Feed extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Feed</Text>
+            </View>
+        )
+}
+}
